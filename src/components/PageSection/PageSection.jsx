5f26b4141816b7bc3c1cc56f658d8336a875100e
Trezy <t@trezy.com>
2023-05-16 15:28:03 -0500

    allow `<PageSection>` to take custom classnames

--- src/components/PageSection/PageSection.jsx
+++ src/components/PageSection/PageSection.jsx
@@ -1,3 +1,12 @@
+// Module imports
+import classnames from 'classnames'
+import PropTypes from 'prop-types'
+import { useMemo } from 'react'
+
+
+
+
+
 // Local imports
 import styles from './PageSection.module.scss'
 
@@ -6,11 +15,28 @@ import styles from './PageSection.module.scss'
 
 
 export function PageSection(props) {
-	const { children } = props
+	const {
+		children,
+		className,
+	} = props
+
+	const compiledClassName = useMemo(() => {
+		return classnames(styles['page-section'], className)
+	}, [className])
 
 	return (
-		<section className={styles['page-section']}>
+		<section className={compiledClassName}>
 			{children}
 		</section>
 	)
+}
+
+PageSection.defaultProps = {
+	children: null,
+	className: '',
+}
+
+PageSection.propTypes = {
+	children: PropTypes.node,
+	className: PropTypes.string,
 }
